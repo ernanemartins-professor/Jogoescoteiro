de7e83b7f344ef16bf893ee8618ed458480c9745
Ernane <ernane.martins@ifg.edu.br>
2026-08-25 16:10:22 -0300

    Corrige caminho do import schema

--- app/api/player/[id]/route.ts
+++ app/api/player/[id]/route.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { db } from '../../src/db/index'
-import { players, achievements, progress } from '../../src/db/schema'
+import { players, achievements, progress } from '../../../src/db/schema'
 import { eq } from 'drizzle-orm'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
