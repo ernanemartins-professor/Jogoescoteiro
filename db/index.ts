967b36685302f841290f5a3900813151a15b404d
Ernane <ernane.martins@ifg.edu.br>
2026-08-30 20:58:45 -0300

    Adiciona SSL na conexão com Supabase

--- db/index.ts
+++ db/index.ts
@@ -15,6 +15,7 @@ export const pool =
   globalForDb.__arenaNextJsPostgresqlPool ??
   new Pool({
     connectionString: databaseUrl,
+    ssl: { rejectUnauthorized: false }, // <- ESSENCIAL para Supabase
   });
 
 if (process.env.NODE_ENV !== "production") {
